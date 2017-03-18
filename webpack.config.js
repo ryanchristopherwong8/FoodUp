7333e9874aa2ef2f8910ff2bf150e366f1aaa3ac
Ryan Wong <ryancwong8@gmail.com>
2017-03-18 16:49:17 -0700

    remove unecessary files

--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,6 @@
 const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: './src/index.html',
-  filename: 'index.html',
-  inject: 'body'
-})
-
 
 module.exports = {
   entry: './src/index.js',
