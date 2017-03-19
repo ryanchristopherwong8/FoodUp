dbc37c973e81d02e430d88165a446ba530a19850
Ryan Wong <ryancwong8@gmail.com>
2017-03-18 22:44:47 -0700

    Add displayReponse component

--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 import IngredientsForm from './components/IngredientsForm.jsx';
+import DisplayResponse from './components/DisplayResponse.jsx';
 import request from 'superagent';
 
 // this renders it to div element root
-ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<IngredientsForm />, document.getElementById('IngredientsForm'));
+
+$(document).ready(function () {
+  ReactDOM.render(<App />, document.getElementById('root'));
+  ReactDOM.render(<IngredientsForm />, document.getElementById('IngredientsForm'));
+  ReactDOM.render(<DisplayResponse />, document.getElementById('DisplayResponse'));
+});
+
 
 $('#hello-world').submit(function(ev) {
     ev.preventDefault(); // to stop the form from submitting
