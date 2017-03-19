61a78ce3504fd076883ba636472ceba60a757ddf
Ryan Wong <ryancwong8@gmail.com>
2017-03-19 12:40:13 -0700

    UI

--- src/components/DisplayResponse.jsx
+++ src/components/DisplayResponse.jsx
@@ -4,18 +4,21 @@ export default class DisplayResponse extends React.Component {
 
   render() {
     return (
-        <div className={'col-md-8'}>
+        <div className={'container-fluid'}>
           {this.props.recipes && this.props.recipes.map(function(object) {
             return (
-              <div>
+              <div className={'container-fluid', 'recipe_block'}>
                 <h2>{object.id}</h2>
-                <h2>{object.name}</h2>
-                <img src={object.image} className={'col-md-6'} style={{display: 'block', width: '500px'}}/>
-                <span><h4>Time:</h4></span><div>{object.time}</div>
-                <h4>Ingredients</h4>
+                <div className={'col-md-8', 'text-center'}><h2 className={'recipe_heading'}>{object.name}</h2></div>
+                <div className={'container-fluid'}>
+                <div className={'col-md-6'}><img src={object.image} className={'img-responsive'} /></div>
+                <div className={'col-md-6'}><h3>Time:</h3>{object.time}</div>
+                <h3>Ingredients</h3>
                 <ul style={{listStyle: 'none'}} className = {'list-group'}>
                     {object.ingredients}
                 </ul>
+                <a href={object.link} className={'btn btn-default btn-lg active'} role="button">Click here to get recipe instructions</a>
+              </div>
               </div>
             );
           })}
