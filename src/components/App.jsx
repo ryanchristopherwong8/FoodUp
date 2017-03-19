40a3f2eeba7ecb66058c66e67dfeef5423d3b99f
Ryan Wong <ryancwong8@gmail.com>
2017-03-18 17:17:07 -0700

    add first react component

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class App extends React.Component {
+  render() {
+    return (
+     <div style={{textAlign: 'center'}}>
+        <h1>Food Up</h1>
+      </div>);
+  }
+}
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
 
-render(
-  <h1>Hello, World!</h1>,
-  document.getElementById('App')
-);
+// this renders it to div element root
+ReactDOM.render(<App />, document.getElementById('root'));
